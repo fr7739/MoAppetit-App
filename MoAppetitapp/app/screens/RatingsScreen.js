d099d1fed61fee77876ae314e38ad35d113d7432
thamima imi <thamimaimi172@gmail.com>
2019-10-23 11:33:29 -0400

    rating working

--- MoAppetitapp/app/screens/RatingsScreen.js
+++ MoAppetitapp/app/screens/RatingsScreen.js
@@ -16,7 +16,7 @@ import { Button } from "react-native-material-ui";
 import styles from "./styles.js";
 import getRatingAPI from "../hasuraAPI/getRatingAPI";
 import setRatingAPI from "../hasuraAPI/setRatingAPI";
-import getProductAPI from "../hasuraAPI/getAllProductAPI";
+import getAllProductsAPI from "../hasuraAPI/getAllProductAPI";
 import setUserAPI from "../hasuraAPI/setUserAPI";
 import getFullUserAPI from "../hasuraAPI/getFullUserAPI";
 import setUseFullInfoAPI from "../hasuraAPI/setUserFullInfoAPI.js";
@@ -36,26 +36,48 @@ export default class RatingsScreen extends React.Component {
     this.ratings = [];
     //this.userState  = {"id:":"0","hasura_id": global.hasura_id, "config": "null", "name":"","phone":"0"};
     if (this.loadRatings()) {
-      console.log("hello");
+      console.log("Ratings Loaded");
+    }
+    if( this.loadAllProducts())
+    {
+      console.log("Products Loaded");
     }
   }
 
   async loadAllProducts() {
     //
     this.allProducts = []; 
-
-    let getRatingResponse = await getRatingAPI(this.state);
-    const resultResponseGetRating = await getRatingResponse.json();
-    for (const key in getRatingResponse) {
+    while(this.allProducts.length > 0) {
+      this.allProducts.pop();
+    }
+    console.log("??????");
+    let getProductsResponse = await getAllProductsAPI(this.state);
+    console.log(getProductsResponse);
+    const resultProducts = await getProductsResponse.json();
+    console.log("sdfsdfsdfsdfsdfjjjjj");
+    for (const key in resultProducts) {
+      console.log("Pushing an item")
+      if(this.allProducts.findIndex( x => x.id == resultProducts[key].id) == -1)
+      {
       this.allProducts.push({
-        id: getRatingResponse[key].id,
-        name: getRatingResponse[key].name
+        id: resultProducts[key].id,
+        name: resultProducts[key].Name
       });
+    }
     }
     this.forceUpdate();
   }
 
   async loadRatings() {
+       if(Array.isArray(this.allProducts) && this.allProducts.length  )
+     {
+      if(this.loadAllProducts())
+      {
+
+      }
+     }
+      
+    
     this.ratings = [];
     const ratingsInfo = {
       user_id: 0
@@ -64,11 +86,15 @@ export default class RatingsScreen extends React.Component {
     let getRatingResponse = await getRatingAPI(ratingsInfo);
     const resultResponseGetRating = await getRatingResponse.json();
     for (const key in resultResponseGetRating) {
+      console.log("iii:"+JSON.stringify(resultResponseGetRating[key]));
+      console.log("Key: "+key);
       this.ratings.push({
         user_id: resultResponseGetRating[key].user_id,
         product_id: resultResponseGetRating[key].product_id,
+        /*Join*/
+        productName:  this.allProducts.find(function(element) { return element.id == resultResponseGetRating[key].product_id}).name,
         id: resultResponseGetRating[key].id,
-        ratingLevel: resultResponseGetRating[key].ratingLevel,
+        ratingLevel: resultResponseGetRating[key].ratinglevel,
         ratingDescription: resultResponseGetRating[key].ratingDescription
       });
     }
@@ -76,19 +102,23 @@ export default class RatingsScreen extends React.Component {
   }
 
   // Handling change when user enters text
-  handlerratinglevelChange = ratinglevel => {
-    this.setState({ ratinglevel });
+  handlerratinglevelChange = (itemValue, itemIndex) => {
+    console.log("Rating itemValue: "+itemValue);
+    this.setState({ratingLevel: itemValue});
   };
 
   handleratingDescriptionChange = ratingDescription => {
     this.state.ratingDescription = String(ratingDescription);
   };
 
-  handleproduct_idChange = product_id => {
-    this.state.product_id = product_id;
+  
+
+  handleproduct_idChange = (itemValue, itemIndex) => {
+    console.log("itemValue: "+itemValue);
+    this.setState({product_id: itemValue});
   };
 
-  handleRatingSubmit = async userInfo => {
+  handleRatingSubmit = async (userInfo) => {
     console.log("Trying to Submit User");
     let RatingResponse = await setRatingAPI(this.state);
     const resultUserResponse = await RatingResponse.json();
@@ -115,13 +145,17 @@ export default class RatingsScreen extends React.Component {
         </View>
       );
     });
-  }
+  } 
 
   render() {
-    let productItems = this.allProducts.map((s, i) => {
-      return <Picker.Item key={i} value={s} label={s} />;
+    console.log(this.allProducts);
+    //if(this.loadAllProducts())
+    //{
+
+    //}
+    let productItems = this.allProducts.map((item) => {
+      return <Picker.Item key={item.id} value={item.id} label={item.name} />;
     });
-    console.log("Count of ratings: " + this.ratings.length);
     return (
       <ImageBackground
         source={require("../assets/OpeningPageBackground.jpg")}
@@ -150,22 +184,20 @@ export default class RatingsScreen extends React.Component {
                 onChangeText={this.handleratingDescriptionChange}
               />
               <Picker
-              label="Rating"
+              label="Rating" 
               mode="dropdown"
               prompt="dialog"
-                selectedValue={this.state.ratingLevel}
+                selectedValue={this.state["ratingLevel"]}
                 style={styles.RatingsfieldsAreaPicker}
                 tintColor="rgba(12, 57, 14, 0.85)"
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({ ratingLevel: itemValue })
-                }
+                onValueChange={this.handlerratinglevelChange}
+                
               >
-                <Picker.Item label="1" value="1" />
-                <Picker.Item label="2" value="2" />
-                <Picker.Item label="3" value="3" />
-                <Picker.Item label="4" value="4" />
-                <Picker.Item label="5" value="5" />
-                {productItems}
+                <Picker.Item key="1" label="1" value="1" />
+                <Picker.Item key="2"  label="2" value="2" />
+                <Picker.Item key="3"  label="3" value="3" />
+                <Picker.Item key="4"  label="4" value="4" />
+                <Picker.Item key="5"  label="5" value="5" />
               </Picker>
 
               <Picker
@@ -173,18 +205,12 @@ export default class RatingsScreen extends React.Component {
               prompt="Product"
                 label="Product"
                 mode="dialog"
-                selectedValue={this.state.product_id}
+                selectedValue={this.state["product_id"]}
                 style={styles.RatingsfieldsAreaPicker}
-                onValueChange={(itemValue, itemIndex) =>
-                  this.setState({ product_id: itemValue })
-                }
+                onValueChange={this.handleproduct_idChange}
+                
               >
                 <Picker.Item label="None" value="0" />
-
-                <Picker.Item label="sdfsdf" value="0" />
-
-                <Picker.Item label="sdfsdf" value="0" />
-                <Picker.Item label="sdfsdf" value="0" />
                 {productItems}
               </Picker>
 
