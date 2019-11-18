1aec2bd6404187e1624fb85829db503255ba4929
Jordan Dickerson <fr7739@wayne.edu>
2019-11-18 06:55:06 -0500

    minor bug fixes

--- MoAppetitapp/app/components/Product.js
+++ MoAppetitapp/app/components/Product.js
@@ -21,14 +21,14 @@ class Prod extends Component {
             return(
               <View key = {product.id}>
                  <Card 
-                    containerStyle = {{height: 250, width: 175, justifyContent: 'center', margin: 0, borderWidth: 0}}
+                    containerStyle = {{height: 300, width: 200, justifyContent: 'center', margin: 0, borderWidth: 0,}}
                  >
                     <TouchableOpacity onPress = { () => this.props.navigation.navigate('Product', {Product: product})}>
                     <Image source={{uri: product.images[0].src}} style = {styles.buttonStyle6} />
                     </TouchableOpacity>
                         <TouchableOpacity
                         onPress = {() => this.props.addItemToCart(product)}
-                        style={{borderRadius: 50, borderWidth: 2,height: 40, width: 40, backgroundColor: '#086522', justifyContent: 'center', alignItems: 'center', position: "absolute", padding:5, right: 1, top: 1}}
+                        style={{borderRadius: 50, borderWidth: 2,height: 40, width: 40, backgroundColor: '#086522', justifyContent: 'center', alignItems: 'center', position: "absolute", padding:4, right: .5, top: 1}}
                         title={product.variants[0].price + "$"}><Text style = {{fontWeight: 'bold', color: 'white', alignContent: 'center', fontSize: 20}}> + </Text></TouchableOpacity>
                         <Text>{product.title}</Text>
                         <Text numberOfLines = {2} style = {{fontSize: 8}}>{product.description}</Text>
@@ -36,13 +36,13 @@ class Prod extends Component {
                 </Card> 
                 <TouchableOpacity
                 onPress = {() => this.props.addItemToCart(product)}
-                style={{borderRadius: 50, borderWidth: 2,height: 40, width: 40, backgroundColor: '#086522', justifyContent: 'center', alignItems: 'center', position: "absolute", padding:5, right: 1, top: 1}}
+                style={{borderRadius: 50, borderWidth: 2,height: 40, width: 40, backgroundColor: '#086522', justifyContent: 'center', alignItems: 'center', position: "absolute", padding:3, right: 1, top: 1}}
                 title={product.variants[0].price + "$"}><Text style = {{fontWeight: 'bold', color: 'white', alignContent: 'center', fontSize: 20}}> + </Text></TouchableOpacity>
                 </View>
             )
         })
         return(
-            <View style = {{flex: 1, justifyContent: 'flex-start', flexDirection: "row", flexWrap: "wrap", backgroundColor: 'white', padding: 0, margin: 0, marginTop: 3, marginBottom: 3, paddingRight: 1, borderWidth: 0, borderColor: 'gray', minWidth: 500}}>
+            <View style = {{flex: 1, justifyContent: 'flex-start', flexDirection: "row", flexWrap: "wrap", backgroundColor: 'white', padding: 0, margin: 0, marginTop: 3, marginBottom: 3, paddingRight: 1, borderWidth: 0, minWidth: 500}}>
             {products}
             </View>
         )
